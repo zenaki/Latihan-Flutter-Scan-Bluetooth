cb257fcde3e2450e787aca3af820ae6e6d247a37
Dendy Gema <dendy.g.arta@gmail.com>
2020-12-03 11:12:06 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/latihan_scan_bluetooth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.latihan_scan_bluetooth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_scan_bluetooth/flutter_scan_bluetooth.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _data = '';
+  bool _scanning = false;
+  FlutterScanBluetooth _bluetooth = FlutterScanBluetooth();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _bluetooth.devices.listen((device) {
+      print("-------------------------Get Device-------------------------");
+      print("Device Name: ${device.name}, Address: ${device.address}");
+      setState(() {
+        _data += device.name + ' (${device.address})\n';
+      });
+    });
+    _bluetooth.scanStopped.listen((device) {
+      setState(() {
+        _scanning = false;
+        _data += 'scan stopped\n';
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(child: Text(_data)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: RaisedButton(
+                    child: Text(_scanning ? 'Stop scan' : 'Start scan'),
+                    onPressed: () async {
+                      try {
+                        if (_scanning) {
+                          await _bluetooth.stopScan();
+                          debugPrint("scanning stoped");
+                          setState(() {
+                            _data = '';
+                          });
+                        } else {
+                          print(
+                              "-------------------------Start Scan-------------------------");
+                          await _bluetooth.startScan(pairedDevices: false);
+                          debugPrint("scanning started");
+                          setState(() {
+                            _scanning = true;
+                          });
+                        }
+                      } on PlatformException catch (e) {
+                        debugPrint(e.toString());
+                      }
+                    }),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
